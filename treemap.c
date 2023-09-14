f7bfff6e9b1683a1c0ed48c88b0adc99cd3647a6
Replit user <>
2023-09-14 19:39:15 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -121,7 +121,7 @@ void removeNode(TreeMap * tree, TreeNode* node) {
       
     }else if(node->left==NULL||node->right==NULL){
       TreeNode* bendicion = (node->left != NULL) ? node->left : node->right;
-      if (node->parent==NULL){
+      if (node->parent!=NULL){
         tree->root=bendicion;
       }else if(node->parent->left==node){
         node->parent=bendicion;
